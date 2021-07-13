9991cbf969b69222fcc896bf3d7b1ec8c96f86f0
Yogendraman <j066037@gmail.com>
2021-07-13 05:42:08 -0400

    ....

---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,5 @@
+#ifndef __FUN_H
+#define __FUN_H
+int marks(float x);
+char* attend(int x);
+#endif
---
+++ 3_Implementation/main.c
@@ -0,0 +1,33 @@
+#include "fun.h"
+#include <stdio.h>
+int main()
+{
+  printf(" Welcome Student, Please enter your roll no.\n");
+  int a,b,c=0,d;
+  scanf(" %d",&a);
+  printf("Enter total number of subjects to calculate marks\n");
+  scanf("%d",&b);
+  float arr1[b];
+  float e=0.00,f,g;
+  while(c!=b){
+      float i,o;
+      printf("Enter marks of subject %d\n",c+1);
+      scanf("%f",&i);
+      o=marks(i);
+      // printf("%.2f\n",o);
+      arr1[c]=o;
+      c=c+1;
+  }
+  printf("Enter your attendance\n");
+  scanf("%d",&d);
+  printf(attend(d)); 
+  for(int i=0;i<=b;i++){
+    e=e+arr1[i];
+  }
+  f=e/(b*10);
+  f=f*100;
+  // printf("%f\n",f);
+  g=marks(f);
+  printf("Your SGPA is %.2f\n",g);
+  return 0;
+}
